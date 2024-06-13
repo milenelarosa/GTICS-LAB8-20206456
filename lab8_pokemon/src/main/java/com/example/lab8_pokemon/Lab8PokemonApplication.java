9f79285263fe5ae847a39d66e15bf05d8093f45e
milenelarosa <a20206456@pucp.edu.pe>
2024-06-12 19:49:34 -0500

    First commit

---
+++ lab8_pokemon/src/main/java/com/example/lab8_pokemon/Lab8PokemonApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab8_pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8PokemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab8PokemonApplication.class, args);
+    }
+
+}
---
+++ lab8_pokemon/src/test/java/com/example/lab8_pokemon/Lab8PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8PokemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
